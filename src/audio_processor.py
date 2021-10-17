4348b45b99c96b6ee334422d99c073d682f7a8a3
Thomas <11610634+twje@users.noreply.github.com>
2021-10-17 18:25:40 +1100

    removed played

--- src/audio_processor.py
+++ src/audio_processor.py
@@ -1,4 +1,4 @@
-from player import Player
+from collections import defaultdict
 
 
 class AudioProcessor:
@@ -8,12 +8,12 @@ class AudioProcessor:
             "on_press": self.on_press,
             "on_release": self.on_release,
         }
-        self.player = Player()
         self.applitude = 2000
         self.time = 0
+        self.tracks = []
 
     def add_track(self, track):
-        self.player.tracks.append(track)
+        self.tracks.append(track)
 
     def on_tick(self, buffer, frame_count, rate, channels):
         # runs in audio thread
@@ -22,12 +22,21 @@ class AudioProcessor:
         for index in range(frame_count):
             tick = self.time + index/rate
             buffer[index] = 0
-            for instrument, notes in self.player.tick(tick):
+            for instrument, notes in self.tick(tick):
                 for note in notes:
                     buffer[index] += instrument.sound(tick, note)
             buffer[index] = int(buffer[index] * self.applitude)
 
         self.time += frame_count/rate
+
+    def tick(self, time):
+        composition = defaultdict(list)
+        for strategy in self.tracks:
+            instrument, notes = strategy.tick(time)
+            composition[instrument].extend(notes)
+
+        for instruemnt, notes in composition.items():
+            yield instruemnt, notes
 
     # --------------
     # Helper Methods
@@ -47,7 +56,9 @@ class AudioProcessor:
     # Callback Methods
     # ----------------
     def on_press(self, note_id, time):
-        self.player.on_press(note_id, time)
+        for track in self.tracks:
+            track.on_press(note_id, time)
 
     def on_release(self, note_id, time):
-        self.player.on_release(note_id, time)
+        for track in self.tracks:
+            track.on_release(note_id, time)
