55a1eff663c2375b922ce0b716540c9e7a608cf5
Thomas <11610634+twje@users.noreply.github.com>
2021-10-24 20:42:42 +1100

    feat: update oscilations

--- src/instrument/oscilations.py
+++ src/instrument/oscilations.py
@@ -1,3 +1,5 @@
+from functools import wraps
+import random
 import math
 
 oscilations = {}
@@ -14,13 +16,64 @@ def hertz_to_angle(hertz):
     return hertz * 2 * math.pi
 
 
-def modulate(time, hertz, lfoHertz, lfoAmplitude):
-    return math.sin(hertz_to_angle(hertz) * time + lfoAmplitude * hertz * math.sin(hertz_to_angle(lfoHertz) * time))
+def modulate(time, hertz, lfo_hertz, lfo_amplitude):
+    return math.sin(hertz_to_angle(hertz) * time + lfo_amplitude * hertz * math.sin(hertz_to_angle(lfo_hertz) * time))
 
 
 @register_oscilation("sine")
-def sine(scale, lfoHertz, lfoAmplitude):
-    # wrap
+def sine(scale, lfo_hertz, lfo_amplitude):
+    @wraps(sine)
     def sound(time, hertz):
-        return scale * math.sin(modulate(time, hertz, lfoHertz, lfoAmplitude))
+        return scale * math.sin(modulate(time, hertz, lfo_hertz, lfo_amplitude))
+    return sound
+
+
+@register_oscilation("square")
+def square(scale, lfo_hertz, lfo_amplitude):
+    @wraps(square)
+    def sound(time, hertz):
+        signal = scale * \
+            math.sin(modulate(time, hertz, lfo_hertz, lfo_amplitude))
+        if signal == 0:
+            return 0
+        elif signal > 0:
+            return 1
+        else:
+            return - 1
+    return sound
+
+
+@register_oscilation("triangle")
+def triangle(scale, lfo_hertz, lfo_amplitude):
+    @wraps(triangle)
+    def sound(time, hertz):
+        return math.asin(math.sin(modulate(time, hertz, lfo_hertz, lfo_amplitude))) * (2/math.pi)
+    return sound
+
+
+@register_oscilation("saw_ana")
+def saw_ana(scale, lfo_hertz, lfo_amplitude):
+    @wraps(saw_ana)
+    def sound(time, hertz):
+        signal = 0
+        frequency = modulate(time, hertz, lfo_hertz, lfo_amplitude)
+        for n in range(1, 100):
+            signal += math.sin(n * frequency)/n
+        return signal * (2/math.pi)
+    return sound
+
+
+@register_oscilation("saw_dig")
+def saw_dig(scale, lfo_hertz, lfo_amplitude):
+    @wraps(saw_dig)
+    def sound(time, hertz):
+        return (2 / math.pi) * (hertz * math.pi * math.fmod(time, 1/hertz) - (math.pi / 2))
+    return sound
+
+
+@register_oscilation("noise")
+def noise(scale, lfo_hertz, lfo_amplitude):
+    @wraps(noise)
+    def sound(time, hertz):
+        return random.uniform(-1, 1)
     return sound
